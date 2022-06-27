262e247bd7f3f8446eccd9f17d89fe74da79a037
ElianY7 <elian.yague@gmail.com>
2022-06-27 11:34:44 -0300

    Commit para prueba de rama

--- mayor_num.py
+++ mayor_num.py
@@ -1,12 +1,10 @@
-def mayor_num(num1,num2,num3,num4,num5):
+def fun_mayor_num(lista):
    
-    lista=[num1,num2,num3,num4,num5]
-
     mayor=0
 
     for i in range (len(lista)):
      if lista[i]>mayor:
-     mayor=lista[i]
+        mayor=lista[i]
 
     print(mayor)
 
